8f7148ad5a25e1c4213c02603e2f1b5662c68ab0
Sankarshan Sharma <161592162+sankarshansharmak81@users.noreply.github.com>
2025-02-06 03:07:55 +0530

    6-2-2025

---
+++ backend/src/utils/asynchandler.js
@@ -0,0 +1,34 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+            next(error);
+        });
+    };
+};
+
+
+export { asyncHandler }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const aysnchandler = (fn) => async (req , res , next) => {
+//     try {
+//         await fn(req, res , next)
+//     } catch (error) {
+//         req.status(error.code || 500).json({
+//             success : false,
+//             message: error.message
+//         })
+        
+//     }
+// }
